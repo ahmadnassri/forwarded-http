46ed4c551eaf021f7451c11d33da533f62212589
Ahmad Nassri <ahmad@codeinchaos.com>
2016-03-23 00:39:21 -0400

    chore(package.json): update dependencies

--- test/index.js
+++ test/index.js
@@ -23,7 +23,7 @@ describe('Forwarded', function () {
   it('should throw an exception when invalid request is passed', function (done) {
     var params
 
-    /*eslint-disable no-extra-parens */
+    /*eslint-disable no-wrap-func */
     (function () {
       params = forwarded({})
     }).should.throw('a request of type: "http.IncomingMessage" is required')
